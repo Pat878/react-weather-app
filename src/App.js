748e2befc73092bfca466a00a05c2ef3c678d01c
Patrick Mellon <mellon.patrick.j@gmail.com>
2017-10-16 19:26:26 -0400

    Add center submit form

--- src/App.js
+++ src/App.js
@@ -1,28 +1,50 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { Navbar, NavItem, NavDropdown, Nav, MenuItem, FormGroup, FormControl,
-  Button } from 'react-bootstrap';
+import { Navbar, FormGroup, FormControl,
+  Button, Form, Col } from 'react-bootstrap';
 
 
   class App extends Component {
     render() {
       return (
-        <Navbar inverse collapseOnSelect>
-          <Navbar.Header>
-            <Navbar.Brand>
-              <a href="#">Weather App</a>
-            </Navbar.Brand>
-            <Navbar.Toggle />
-          </Navbar.Header>
-          <Navbar.Form pullRight>
-            <FormGroup>
-              <FormControl type="text" placeholder="Lancaster, PA" />
-            </FormGroup>
-            {' '}
-            <Button type="submit">Get Weather</Button>
-          </Navbar.Form>
-        </Navbar>
+
+        <div>
+          <Navbar inverse collapseOnSelect>
+            <Navbar.Header>
+              <Navbar.Brand>
+                Weather App
+              </Navbar.Brand>
+              <Navbar.Toggle />
+            </Navbar.Header>
+            <Navbar.Form pullRight>
+              <FormGroup>
+                <FormControl type="text" placeholder="Lancaster, PA" />
+              </FormGroup>
+              {' '}
+              <Button type="submit">Get Weather</Button>
+            </Navbar.Form>
+          </Navbar>
+
+          <div className="weather">
+            <center>
+              <div id="title">Enter a City and a State</div><br/>
+              <Form horizontal>
+                <FormGroup controlId="formHorizontalLocation">
+                  <Col smOffset={5} sm={2}>
+                    <FormControl type="location" placeholder="Lancaster, PA" />
+                  </Col>
+                </FormGroup>
+                <FormGroup>
+                  <Button type="submit">
+                    Get Weather
+                  </Button>
+                </FormGroup>
+              </Form>
+            </center>
+          </div>
+        </div>
+
       );
     }
   }
