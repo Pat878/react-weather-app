41bb9a4d8af04a6579404a207880da061b3c4319
Patrick Mellon <mellon.patrick.j@gmail.com>
2017-10-16 18:32:07 -0400

    Add navbar

--- src/App.js
+++ src/App.js
@@ -1,21 +1,30 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Navbar, NavItem, NavDropdown, Nav, MenuItem, FormGroup, FormControl,
+  Button } from 'react-bootstrap';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+
+  class App extends Component {
+    render() {
+      return (
+        <Navbar inverse collapseOnSelect>
+          <Navbar.Header>
+            <Navbar.Brand>
+              <a href="#">Weather App</a>
+            </Navbar.Brand>
+            <Navbar.Toggle />
+          </Navbar.Header>
+          <Navbar.Form pullRight>
+            <FormGroup>
+              <FormControl type="text" placeholder="Lancaster, PA" />
+            </FormGroup>
+            {' '}
+            <Button type="submit">Get Weather</Button>
+          </Navbar.Form>
+        </Navbar>
+      );
+    }
   }
-}
 
-export default App;
+  export default App;
