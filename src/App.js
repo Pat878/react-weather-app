7b5522de1cc462a53fdea259e99393c18903d651
Patrick Mellon <mellon.patrick.j@gmail.com>
2018-05-26 09:36:38 -0400

    Move DayDetail and FiveDayForecast components

--- src/App.js
+++ src/App.js
@@ -18,6 +18,8 @@ var axios = require("axios");
 var Loading = require("./Loading");
 var WeatherNav = require("./components/WeatherNav");
 var Input = require("./components/Input");
+var FiveDayForecast = require("./components/FiveDayForecast");
+var DayDetail = require("./components/DayDetail");
 
 class App extends Component {
   state = {
@@ -141,88 +143,5 @@ class App extends Component {
     );
   }
 }
-
-const FiveDayForecast = props => {
-  const weatherObject = props.weather.list;
-  const dates = [];
-  const imageIcon = [];
-
-  const mappedWeatherObject = Object.keys(weatherObject).map((key, index) => {
-    let options = { weekday: "long", month: "long", day: "numeric" };
-
-    dates.push(
-      new Date(weatherObject[index].dt * 1000).toLocaleDateString(
-        "en-US",
-        options
-      )
-    );
-    imageIcon.push(weatherObject[index].weather);
-
-    return (
-      <div key={index} onClick={props.viewDayDetail.bind(this, index)}>
-        <div>
-          <img
-            alt="weather_icon"
-            src={
-              "http://openweathermap.org/img/w/" +
-              imageIcon[key][0].icon +
-              ".png"
-            }
-          />
-        </div>
-        <div>
-          <h3>{dates[key]}</h3>
-        </div>
-      </div>
-    );
-  });
-
-  return (
-    <div>
-      <center>
-        <h1>{props.weather.city.name}</h1>
-        <div>{mappedWeatherObject}</div>
-      </center>
-    </div>
-  );
-};
-
-const DayDetail = props => {
-  let weatherProps = props.weather;
-  let options = { weekday: "long", month: "long", day: "numeric" };
-  let index = props.detailIndex;
-  let date = new Date(weatherProps.list[index].dt * 1000).toLocaleDateString(
-    "en-US",
-    options
-  );
-  let icon = weatherProps.list[index].weather[0].icon;
-  let city = weatherProps.city.name;
-  let description = weatherProps.list[index].weather[0].description;
-  let minTemp = Math.round(weatherProps.list[index].temp.min * 9 / 5 - 459.67);
-  let maxTemp = Math.round(weatherProps.list[index].temp.max * 9 / 5 - 459.67);
-  let humidity = weatherProps.list[index].humidity;
-
-  return (
-    <div>
-      <center>
-        <div>
-          {
-            <img
-              alt="weather_icon"
-              src={"http://openweathermap.org/img/w/" + icon + ".png"}
-            />
-          }
-        </div>
-        <h2>{date}</h2>
-        <h2>{city}</h2>
-        <h3>{description}</h3>
-        <h3>Min Temp: {minTemp} degrees</h3>
-        <h3>Max Temp: {maxTemp} degrees</h3>
-        <h3>Humidity: {humidity}</h3>
-        <Button onClick={props.goBack}>Back</Button>
-      </center>
-    </div>
-  );
-};
 
 export default App;
